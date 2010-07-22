afe9aa008a478e38229adb5710547c33b84beed0
Chuck Collins <chuck.collins@gmail.com>
2010-07-22 10:33:16 -0500

    moved pidme.py location

--- pidme.py
+++ pidme.py
@@ -2,12 +2,12 @@ import os
 import atexit
 
 def register_pidfile(pidfile):
-    if os.path.isfile(pid):
+    if os.path.isfile(pidfile):
         return False
     
     file(pidfile, 'w').write(str(os.getpid()))
-    atexit.register(clear_pid, pidfile=pidfile)
+    atexit.register(clear_pidfile, pidfile=pidfile)
     return True
     
 def clear_pidfile(pidfile):
-    os.remove(path)
+    os.remove(pidfile)
